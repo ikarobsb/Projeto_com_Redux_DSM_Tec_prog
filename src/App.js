e435b11164f9fc98ed387bb3ba6d5f4b6719d22a
ikarobsb <88170008+ikarobsb@users.noreply.github.com>
2022-04-28 22:20:25 -0300

    Card e Intervalo (components) e CSS

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from "react";
 import "./App.css";
 import Card from "./components/Card";
+import Intervalo from "./components/Intervalo";
 
 function App() {
   return (
     <div className="App">
       <h1>Projeto Redux</h1>
       <div className="linha">
-        <Card />
+        <Intervalo />
       </div>
       <div className="linha">
-        <Card />
+      <Card title="Card 1" blue> Conteúdo </Card>
+      <Card title="Card 2" green> Conteúdo </Card>
+      <Card title="Card 3" purple> Conteúdo </Card>
       </div>
     </div>
   );
